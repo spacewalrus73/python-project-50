d580b42475b964599c27564e0e58dc00fbb4e4fb
Daniil Balabanov <spacewalrus@bk.ru>
2023-02-27 15:58:13 +0300

    make help list with argparse

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,7 +1,11 @@
 import argparse
 
-def argparse_help():
-    parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and show a difference.')
-    parser.add_argument(dest='first_file')
-    parser.add_argument(dest='second_file')
-    return parser.parse_args(['-h'])
+
+def parse_help():
+    parser = argparse.ArgumentParser(prog='gendiff',
+                                     description='Compares two configuration files and show a difference.')
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+
