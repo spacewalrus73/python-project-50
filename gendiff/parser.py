1bac931e537190f02dbd3f5d6c7b86eed0497d6b
Daniil Balabanov <spacewalrus@bk.ru>
2023-03-24 20:50:58 +0300

    upgrade funtion to_parse

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,7 +1,7 @@
 import argparse
-from os import path
 from gendiff.gendiff_main import generate_diff
 from gendiff import encode
+
 
 parser = argparse.ArgumentParser(
     prog='gendiff',
@@ -20,11 +20,17 @@ parser.add_argument(
 def to_parse():
     args = parser.parse_args()
     dirs = [args.first_file, args.second_file]
-    input_format = path.splitext(dirs[0])[1]
-
-    if input_format == '.json':
+    if str(dirs[0]).endswith('.json') and str(dirs[1]).endswith('.json'):
         files_lst = list(map(encode.from_json_to_dict, dirs))
         print(generate_diff(files_lst[0], files_lst[1], args.format))
+    elif str(dirs[0]).endswith('.json'):
+        file_json = encode.from_json_to_dict(dirs[0])
+        file_yaml = encode.from_yaml_to_dict(dirs[1])
+        print(generate_diff(file_json, file_yaml, args.format))
+    elif str(dirs[1]).endswith('.json'):
+        file_json = encode.from_json_to_dict(dirs[1])
+        file_yaml = encode.from_yaml_to_dict(dirs[0])
+        print(generate_diff(file_json, file_yaml, args.format))
     else:
         files_lst = list(map(encode.from_yaml_to_dict, dirs))
         print(generate_diff(files_lst[0], files_lst[1], args.format))
