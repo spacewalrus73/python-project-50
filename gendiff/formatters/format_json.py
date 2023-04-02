1c62dab060757b16fc5b3c63d30f3fdabc12cc6d
Daniil Balabanov <spacewalrus@bk.ru>
2023-04-02 04:39:36 +0300

    fixed json dumps

--- gendiff/formatters/format_json.py
+++ gendiff/formatters/format_json.py
@@ -2,7 +2,4 @@ from json import dumps
 
 
 def json(diff):
-    result = ''
-    for item in diff:
-        result = result + dumps(item, indent=4) + '\n'
-    return result[:-1]
+    return dumps(diff, indent=4)
