4d148b845766a7e7e35e4469447439ab527ab3f1
Daniil Balabanov <spacewalrus@bk.ru>
2023-04-09 06:07:06 +0300

    arg parser now in a single finction

--- gendiff/argparser.py
+++ gendiff/argparser.py
@@ -4,16 +4,13 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(
         prog='gendiff',
-        description='Compares two configuration files and show a difference.'
-    )
+        description='Compares two configuration files and show a difference.')
 
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument(
-        '-f', '--format',
-        help='set format of output',
-        default='stylish'
-    )
+    parser.add_argument('-f', '--format',
+                        help='set format of output',
+                        default='stylish')
     args = parser.parse_args()
 
     return args.first_file, args.second_file, args.format
