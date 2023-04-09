6baff958d1afa4fbe5a9fa91e994aca631a46c2f
Daniil Balabanov <spacewalrus@bk.ru>
2023-04-09 06:14:08 +0300

    test data moved to test module, tests simplified

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,29 +1,22 @@
 import pytest
-from gendiff.generate_diff import generate_diff, is_dicts
-from tests import test_data
+from gendiff.generate_diff import generate_diff
 
 
-def test_is_dicts():
-    test_lst = [{}, ['1'], {'k': 'v'}, {}, [], None]
-
-    assert is_dicts(test_lst[0], test_lst[1]) is False
-    assert is_dicts(test_lst[2], test_lst[3]) is True
-    assert is_dicts(test_lst[4], test_lst[5]) is False
-
-
-@pytest.mark.parametrize("file1, file2, expected, style", test_data.data_json)
-def test_generate_diff_json(file1, file2, expected, style):
-    with open(expected, 'r') as f:
-        assert f.read() == generate_diff(file1, file2, style)
-
-
-@pytest.mark.parametrize("file1, file2, expected, style", test_data.data_yml)
-def test_generate_diff_yml(file1, file2, expected, style):
-    with open(expected, 'r') as f:
-        assert f.read() == generate_diff(file1, file2, style)
+data = [('tests/fixtures/example_json/file1_nested.json',
+         'tests/fixtures/example_json/file2_nested.json',
+         'tests/fixtures/expected_results/nested_stylish.txt',
+         'stylish'),
+        ('tests/fixtures/example_yml/file1_flat.yml',
+         'tests/fixtures/example_yml/file2_flat.yml',
+         'tests/fixtures/expected_results/plain_plain.txt',
+         'plain'),
+        ('tests/fixtures/example_yml/nested.yaml',
+         'tests/fixtures/example_json/file2_nested.json',
+         'tests/fixtures/expected_results/nested_json.txt',
+         'json')]
 
 
-@pytest.mark.parametrize("file1, file2, expected, style", test_data.data_mixed)
-def test_generate_diff_mixed(file1, file2, expected, style):
-    with open(expected, 'r') as f:
+@pytest.mark.parametrize("file1, file2, expected_result, style", data)
+def test_generate_diff_json(file1, file2, expected_result, style):
+    with open(expected_result, 'r') as f:
         assert f.read() == generate_diff(file1, file2, style)
